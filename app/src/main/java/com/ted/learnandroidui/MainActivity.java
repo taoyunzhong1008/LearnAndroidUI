7a5b4386793735477888deaa6a93e6c759b86430
taoyunzhong <taoyunzhong1008@gmail.com>
2017-03-01 23:30:52 +0800

    Test RandomNumberView

--- app/src/main/java/com/ted/learnandroidui/MainActivity.java
+++ app/src/main/java/com/ted/learnandroidui/MainActivity.java
@@ -2,12 +2,22 @@ package com.ted.learnandroidui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import customview.RandomNumberView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RandomNumberView mRandowNumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mRandowNumber = (RandomNumberView) findViewById(R.id.custom_random_number);
     }
 }
