04e9533571569b4779dd623dd480e6fec1082b92
taoyunzhong <taoyunzhong1008@gmail.com>
2017-03-02 22:53:41 +0800

    add Base

---
+++ app/src/main/java/com/ted/learnandroidui/base/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.ted.learnandroidui.base;
+
+/**
+ * FileName:
+ * com.ted.learnandroidui.base.BaseActivity.java
+ * Author: TT
+ * Date: 2017-03-02
+ * Description: <描述当前版本功能>
+ */
+public class BaseActivity {
+}
---
+++ app/src/main/java/com/ted/learnandroidui/base/MyApplication.java
@@ -0,0 +1,14 @@
+package com.ted.learnandroidui.base;
+
+import android.app.Application;
+
+/**
+ * FileName:
+ * com.ted.learnandroidui.base.MyApplication.java
+ * Author: TT
+ * Date: 2017-03-02
+ * Description: <描述当前版本功能>
+ */
+public class MyApplication extends Application{
+
+}
