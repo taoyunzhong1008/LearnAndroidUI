4230797df611d9a977fe48d97704357a95829997
taoyunzhong <taoyunzhong1008@gmail.com>
2017-03-04 23:14:18 +0800

    update BaseActivity

--- app/src/main/java/com/ted/learnandroidui/base/BaseActivity.java
+++ app/src/main/java/com/ted/learnandroidui/base/BaseActivity.java
@@ -1,5 +1,7 @@
 package com.ted.learnandroidui.base;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -11,4 +13,8 @@ import android.support.v7.app.AppCompatActivity;
  */
 public class BaseActivity extends AppCompatActivity{
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
