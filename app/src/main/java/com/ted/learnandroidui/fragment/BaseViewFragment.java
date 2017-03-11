57ba7c41f2bd2edc8df7e1dc467c4fc5d85ad1b6
taoyunzhong <taoyunzhong1008@gmail.com>
2017-03-11 23:38:56 +0800

    update

--- app/src/main/java/com/ted/learnandroidui/fragment/BaseViewFragment.java
+++ app/src/main/java/com/ted/learnandroidui/fragment/BaseViewFragment.java
@@ -1,5 +1,11 @@
 package com.ted.learnandroidui.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import com.ted.learnandroidui.base.BaseFragment;
 
 /**
@@ -10,4 +16,9 @@ import com.ted.learnandroidui.base.BaseFragment;
  * Description: <描述当前版本功能>
  */
 public class BaseViewFragment extends BaseFragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
