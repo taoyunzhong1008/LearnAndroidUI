f02baa115dcaab0a4d6416c0acb25cffcd6db514
taoyunzhong1008 <taoyunzhong1008@gmail.com>
2017-03-17 23:43:12 +0800

    update

--- app/src/main/java/com/ted/learnandroidui/fragment/BaseViewFragment.java
+++ app/src/main/java/com/ted/learnandroidui/fragment/BaseViewFragment.java
@@ -5,9 +5,13 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.ted.learnandroidui.R;
 import com.ted.learnandroidui.base.BaseFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * FileName:
@@ -17,10 +21,15 @@ import com.ted.learnandroidui.base.BaseFragment;
  * Description: <描述当前版本功能>
  */
 public class BaseViewFragment extends BaseFragment{
+
+    @BindView(R.id.lv_views)
+    ListView mLvViews;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_base_view, container, false);
+        ButterKnife.bind(this, view);
         return view;
     }
 
