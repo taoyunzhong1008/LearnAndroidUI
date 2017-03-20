7cb4355fc5893b8f3d7eb75c98717fd5aeaf8f98
taoyunzhong1008 <taoyunzhong1008@gmail.com>
2017-03-20 21:48:42 +0800

    update

--- app/src/main/java/com/ted/learnandroidui/base/BaseFragment.java
+++ app/src/main/java/com/ted/learnandroidui/base/BaseFragment.java
@@ -2,7 +2,12 @@ package com.ted.learnandroidui.base;
 
 
 import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * FileName:
@@ -15,5 +20,11 @@ public class BaseFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
